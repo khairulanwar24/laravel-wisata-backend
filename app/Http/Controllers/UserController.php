30f2f58f679ff72280264b62f7d7606aa9cbc6d4
Muhammad Khairul Anwar <khairul240102@gmail.com>
2024-05-30 21:32:22 +0700

    add users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,4 +12,24 @@ class UserController extends Controller
         $users = User::paginate(10);
         return view('pages.users.index', compact('users')); // butuh folder users dan file index
     }
+
+    // create user
+    public function create() {
+        return view('pages.users.create');
+    }
+
+    // store
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'phone' => 'required',
+            'role' => 'required',
+        ]);
+
+        User::create($request->all());
+        return redirect()->route('users.index')->with('success', 'User created successfully');
+    }
 }
