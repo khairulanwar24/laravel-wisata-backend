19d9ed8e1d12173b793fbc1559a2c36febd23ba2
Muhammad Khairul Anwar <khairul240102@gmail.com>
2024-06-07 01:13:18 +0700

    store api products

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -15,4 +15,40 @@ class ProductController extends Controller
         })->orderBy('favorite', 'desc')->get();
         return response()->json(['status'=>'success', 'data' => $products], 200);
     }
+
+    // store
+    public function store(Request $request) {
+        $request->validate([
+            'category_id' => 'required',
+            'name' => 'required',
+            'price' => 'required',
+            'image' => 'required',
+            'criteria' => 'required',
+            'favorite' => 'required',
+            'status' => 'required',
+            'stock' => 'required',
+        ]);
+
+        $product = new Product;
+        $product->category_id = $request->category_id;
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->criteria = $request->criteria;
+        $product->favorite = $request->favorite;
+        $product->status = $request->status;
+        $product->stock = $request->stock;
+        $product->save();
+
+        // upload image
+        if ($request->file('image')) {
+            $image = $request->file('image');
+            $image->storeAs('public/products', $product->id . '.png');
+            $product->image = $product->id . '.png';
+            $product->save();
+        }
+
+        return response()->josn(['status' => 'success', 'data' => $product], 200);
+
+    }
 }
